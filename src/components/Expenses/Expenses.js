1b7e06af1d76fdf0e642266f63c8c22ff51c96f3
Akash Sinha <akashsinha0807@outlook.com>
2022-09-01 20:04:11 +0530

    Assignment 3

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,16 +8,16 @@ import { useState } from "react";
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
   const setFilterHandler = (enteredFilterData) => {
-    console.log(enteredFilterData);
     setFilteredYear(enteredFilterData);
   };
+  const filteredExpenses = props.items.filter(x=>x.date.getFullYear().toString() === filteredYear);
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onSettingFilter={setFilterHandler}
       />
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key= {expense.id}
           title={expense.title}
